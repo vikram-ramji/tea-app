3ba6e0de5cce9229c7c351fe403ae6cf7e8e7b17
Vikram Ramji Iyer <vikramramji24@gmail.com>
2025-02-19 16:18:13 +0530

    add to github

---
+++ index.js
@@ -0,0 +1,54 @@
+import 'dotenv/config'
+import express from "express"
+
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+let teaData = []
+let nextId = 1
+
+app.post('/tea', (req, res) => {
+    const {name, price} = req.body
+    const newTea = {id: nextId++, name, price}
+    teaData.push(newTea)
+    res.status(200).send(newTea)
+})
+
+app.get('/tea', (req, res) => {
+    res.status(200).send(teaData)
+})
+
+app.get('/tea/:id', (req, res) => {
+    const tea = teaData.find(t => t.id === parseInt(req.params.id))
+    if (!tea) {
+        return res.status(404).send('Tea not found')
+    }
+    res.status(200).send(tea)
+})
+
+app.put('/tea/:id', (req, res) => {
+    const tea = teaData.find(t => t.id === parseInt(req.params.id))
+
+    if (!tea) {
+        return res.status(404).send('Tea not found')
+    }
+    const {name, price} = req.body
+    tea.name = name
+    tea.price = price
+    res.status(200).send(tea)
+})
+
+app.delete('/tea/:id', (req, res) => {
+    const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
+    if (index === -1) {
+        return res.status(404).send('tea not found')
+    }
+    teaData.splice(index, 1)
+    return res.status(200).send('deleted')
+})
+
+
+app.listen(port,() => {
+    console.log(`Server is running at port ${port}...`)
+})
